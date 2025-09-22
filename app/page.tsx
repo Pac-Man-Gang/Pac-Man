12e1b15e48bf568e84f163a48df3742da676e3c0
Joel Bonini <joel.bonini@gmx.ch>
2025-09-22 11:47:49 +0200

    Added return statement

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 
 export default function HomePage() {
-  <main>
-    <Link href="/game">
-      <button>Game</button>
-    </Link>
-    <p>This is the home page</p>
-  </main>
+  return (
+    <main>
+      <Link href="/game">
+        <button>Game</button>
+      </Link>
+      <p>This is the home page</p>
+    </main>
+  );
 }
