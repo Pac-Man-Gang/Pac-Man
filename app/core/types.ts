5b4977a387b25fce653630f9010948e0f262c6c3
bisqwitt <yuuki.aschwanden04@gmail.com>
2025-09-26 14:37:39 +0200

    Run ESlint

--- app/core/types.ts
+++ app/core/types.ts
@@ -1,22 +1,41 @@
-export type Position = { x: number, y: number }
-export enum Direction { N, S, W, E }
+export type Position = { x: number; y: number };
+export enum Direction {
+  N,
+  S,
+  W,
+  E,
+}
 
-export type Entity = { pos: Position, sprite: string }
-export type MoveableEntity = Entity & { dir: Direction, frame: number, spawnPoint: Position }
+export type Entity = { pos: Position; sprite: string };
+export type MoveableEntity = Entity & {
+  dir: Direction;
+  frame: number;
+  spawnPoint: Position;
+};
 
-export type PacMan = MoveableEntity & {}
+export type PacMan = MoveableEntity & {};
 
-export enum GhostMode { CHASE, SCATTER, FRIGHTENED, EATEN }
-export enum GhostType { BLINKY, PINKY, INKY, CLYDE }
-export type Ghost = MoveableEntity & { type: GhostType, mode: GhostMode }
+export enum GhostMode {
+  CHASE,
+  SCATTER,
+  FRIGHTENED,
+  EATEN,
+}
+export enum GhostType {
+  BLINKY,
+  PINKY,
+  INKY,
+  CLYDE,
+}
+export type Ghost = MoveableEntity & { type: GhostType; mode: GhostMode };
 
-export type Pellet = Entity & {}
-export type SuperPellet = Pellet & {}
+export type Pellet = Entity & {};
+export type SuperPellet = Pellet & {};
 
 export type GameState = {
-    pacman: PacMan,
-    ghosts: Ghost[],
-    pellets: Pellet[],
-    score: number,
-    lives: number
-}
+  pacman: PacMan;
+  ghosts: Ghost[];
+  pellets: Pellet[];
+  score: number;
+  lives: number;
+};
