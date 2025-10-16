1dbef6df1b7ac942eeaf86b960e6b174041b87de
kazee72 <joel.bonini@gmx.ch>
2025-10-16 16:05:57 +0200

    Refactored most of the maze gen still missing inside corners

--- app/(ui)/game/MazeLayer.tsx
+++ app/(ui)/game/MazeLayer.tsx
@@ -20,132 +20,283 @@ import SuperPelletSprite, {
 // Out of Map = 3
 // Ghost House = 4
 // Super Pellets = 5
+// Tunnel = 6
+
+// prettier-ignore
 export const LEVEL_MAP: number[][] = [
-  [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1,
-  ],
-  [
-    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 5, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    1, 5, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2,
-    2, 2, 1,
-  ],
-  [
-    1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    1, 1, 1,
-  ],
-  [
-    3, 3, 3, 3, 3, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 3, 3,
-    3, 3, 3,
-  ],
-  [
-    3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 3, 3,
-    3, 3, 3,
-  ],
-  [
-    3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 1, 1, 1, 4, 4, 1, 1, 1, 0, 1, 1, 2, 1, 3, 3,
-    3, 3, 3,
-  ],
-  [
-    1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 1, 1, 2, 1, 1, 1,
-    1, 1, 1,
-  ],
-  [
-    0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 0, 0, 2, 0, 0, 0,
-    0, 0, 0,
-  ],
-  [
-    1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 1, 1, 2, 1, 1, 1,
-    1, 1, 1,
-  ],
-  [
-    3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 3, 3,
-    3, 3, 3,
-  ],
-  [
-    3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 3, 3,
-    3, 3, 3,
-  ],
-  [
-    3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 3, 3,
-    3, 3, 3,
-  ],
-  [
-    1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1,
-    1, 1, 1,
-  ],
-  [
-    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 5, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2,
-    2, 5, 1,
-  ],
-  [
-    1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2,
-    1, 1, 1,
-  ],
-  [
-    1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2,
-    1, 1, 1,
-  ],
-  [
-    1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2,
-    2, 2, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 2, 1,
-  ],
-  [
-    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 1,
-  ],
-  [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1,
-  ],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+  [1, 5, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 5, 1],
+  [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+  [1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+  [3, 3, 3, 3, 3, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 3, 3, 3, 3, 3],
+  [3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 3, 3, 3, 3, 3],
+  [3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 1, 1, 1, 4, 4, 1, 1, 1, 0, 1, 1, 2, 1, 3, 3, 3, 3, 3],
+  [1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+  [6, 6, 6, 6, 6, 6, 2, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 0, 0, 2, 6, 6, 6, 6, 6, 6],
+  [1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 4, 4, 4, 4, 4, 4, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+  [3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 3, 3, 3, 3, 3],
+  [3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 3, 3, 3, 3, 3],
+  [3, 3, 3, 3, 3, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 3, 3, 3, 3, 3],
+  [1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+  [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+  [1, 5, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 5, 1],
+  [1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1],
+  [1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+  [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+enum ComponentType {
+  Wall,
+  Corner,
+  Smallpellet,
+  Superpellet,
+  Ghosthouse,
+  Void,
+  Empty,
+}
+
+const DIRS = {
+  N: [-1, 0],
+  S: [1, 0],
+  W: [0, -1],
+  E: [0, 1],
+  NE: [-1, 1],
+  NW: [-1, -1],
+  SE: [1, 1],
+  SW: [1, -1],
+} as const;
+
+class MazeComponent {
+  row: number;
+  col: number;
+  type: ComponentType;
+  component: ReactElement;
+  neighbours: Record<string, number | undefined>;
+  isNeighbourWall: Record<string, boolean>;
+
+  constructor(row: number, col: number) {
+    this.row = row;
+    this.col = col;
+    this.neighbours = Object.fromEntries(
+      Object.entries(DIRS).map(([key, [r, c]]) => [
+        key,
+        LEVEL_MAP[row + r]?.[col + c],
+      ])
+    );
+    this.isNeighbourWall = Object.fromEntries(
+      Object.entries(this.neighbours).map(([key, value]) => [key, value === 1])
+    );
+    this.type = this.defineComponentType();
+    this.component = this.defineComponent();
+  }
+
+  defineComponent(): ReactElement {
+    switch (this.type) {
+      case ComponentType.Empty:
+      case ComponentType.Ghosthouse:
+      case ComponentType.Void:
+        return <EmptyCell />;
+      case ComponentType.Smallpellet:
+        return <SmallPelletSprite row={this.row} col={this.col} />;
+      case ComponentType.Superpellet:
+        return <SuperPelletSprite row={this.row} col={this.col} />;
+      case ComponentType.Corner:
+        return this.defineCorner();
+      case ComponentType.Wall:
+        return this.defineWall();
+      default:
+        return <EmptyCell />;
+    }
+  }
+
+  defineWall(): ReactElement {
+    if (Object.values(this.neighbours).every((n) => n === 1)) {
+      return <EmptyCell />;
+    }
+
+    if (this.isVertical()) {
+        if ([3, 4, undefined].includes(this.neighbours.E)) {
+            return <DoubleWall mirrored={true}/>;
+        } else if (Object.values(this.neighbours).some(n => n === 3 || n === 4 || n === undefined)) {
+            return <DoubleWall />;
+        } else {
+            if (this.neighbours.E === 2 || this.neighbours.E === 0 || this.neighbours.E === 5) {
+                return <SingleWall mirrored={true} />;
+            } else {
+                return <SingleWall />;
+            }
+        }
+    } else {
+        if ([3, 4, undefined].includes(this.neighbours.S)) {
+            return <DoubleWall rotation={270} mirrored={true} />;
+        } else if (Object.values(this.neighbours).some(n => n === 3 || n === 4 || n === undefined)){
+            return <DoubleWall rotation={90} />;
+        } else {
+            if (this.neighbours.S === 2 || this.neighbours.S === 0 || this.neighbours.S === 5) {
+                return <SingleWall rotation={270} mirrored={true} />;
+            } else {
+                return <SingleWall rotation={90} />;
+            }
+        }
+        
+    }
+
+  }
+
+  isVertical(): boolean {
+    return this.isNeighbourWall.N && this.isNeighbourWall.S;
+  }
+
+  defineCorner(): ReactElement {
+    // Corner (N to E)
+    if (
+      this.isNeighbourWall.N &&
+      this.isNeighbourWall.E &&
+      !this.isNeighbourWall.S &&
+      !this.isNeighbourWall.W
+    ) {
+      if (this.neighbours.S === undefined || this.neighbours.S === 3) {
+        return <DoubleCorner/>;
+      } else if (this.neighbours.NE === 3) {
+        return <ShortCorner/>;
+      } else if (this.neighbours.NE === 4) {
+        return <SharpDoubleCorner/>;
+      } else {
+        return <SingleCorner/>;
+      }
+      // Corner (N to W)
+    } else if (
+      this.isNeighbourWall.N &&
+      this.isNeighbourWall.W &&
+      !this.isNeighbourWall.S &&
+      !this.isNeighbourWall.E
+    ) {
+      if (this.neighbours.S === undefined || this.neighbours.S === 3) {
+        return <DoubleCorner rotation={270}/>;
+      } else if (this.neighbours.NW === 3) {
+        return <ShortCorner rotation={270}/>;
+      } else if (this.neighbours.NW === 4) {
+        return <SharpDoubleCorner rotation={270}/>;
+      } else {
+        return <SingleCorner rotation={270}/>;
+      }
+      // Corner (S to E)
+    } else if (
+      this.isNeighbourWall.S &&
+      this.isNeighbourWall.E &&
+      !this.isNeighbourWall.N &&
+      !this.isNeighbourWall.W
+    ) {
+      if (this.neighbours.N === undefined || this.neighbours.N === 3) {
+        return <DoubleCorner rotation={90} />;
+      } else if (this.neighbours.SE === 3) {
+        return <ShortCorner rotation={90} />;
+      } else if (this.neighbours.SE === 4) {
+        return <SharpDoubleCorner rotation={90} />;
+      } else {
+        return <SingleCorner rotation={90}/>;        
+      }
+      // Corner (S to W)
+    } else if (
+      this.isNeighbourWall.S &&
+      this.isNeighbourWall.W &&
+      !this.isNeighbourWall.N &&
+      !this.isNeighbourWall.E
+    ) {
+      if (this.neighbours.N === undefined || this.neighbours.N === 3) {
+        return <DoubleCorner rotation={180} />;
+      } else if (this.neighbours.SW === 3) {
+        return <ShortCorner rotation={180} />;
+      } else if (this.neighbours.SW === 4) {
+        return <SharpDoubleCorner rotation={180} />;
+      } else {
+        return <SingleCorner rotation={180}/>;
+      }
+    }
+
+    return <SingleCorner />;
+  }
+
+  defineComponentType(): ComponentType {
+    switch (true) {
+      case this.isCorner():
+        return ComponentType.Corner;
+      case this.isWall():
+        return ComponentType.Wall;
+      case this.isSmallPellet():
+        return ComponentType.Smallpellet;
+      case this.isSuperPellet():
+        return ComponentType.Superpellet;
+      case this.isEmpty():
+        return ComponentType.Empty;
+      case this.isVoid():
+        return ComponentType.Void;
+      case this.isGhostHouse():
+        return ComponentType.Ghosthouse;
+      default:
+        return ComponentType.Empty;
+    }
+  }
+
+  isWall(): boolean {
+    return LEVEL_MAP[this.row][this.col] === 1 && !this.isCorner();
+  }
+
+  isCorner(): boolean {
+  const w = this.isNeighbourWall;
+  return (
+    LEVEL_MAP[this.row][this.col] === 1 &&
+    (
+      // Outside corners (L-shaped) - exactly 2 perpendicular walls
+      (w.N && w.E && !w.S && !w.W) ||
+      (w.N && w.W && !w.S && !w.E) ||
+      (w.S && w.E && !w.N && !w.W) ||
+      (w.S && w.W && !w.N && !w.E) ||
+      // Inside corners - check specific diagonal based on which walls exist
+      // Horizontal walls with at least one vertical wall and a missing diagonal
+      (w.E && w.W && w.N && !w.S && (!w.NE || !w.NW)) ||
+      (w.E && w.W && w.S && !w.N && (!w.SE || !w.SW)) ||
+      // Vertical walls with at least one horizontal wall and a missing diagonal
+      (w.N && w.S && w.E && !w.W && (!w.NE || !w.SE)) ||
+      (w.N && w.S && w.W && !w.E && (!w.NW || !w.SW)) ||
+      // Cross shape (all 4 walls) with at least one missing diagonal
+      (w.N && w.S && w.E && w.W && (!w.NE || !w.NW || !w.SE || !w.SW))
+    )
+  );
+}
+
+  isSmallPellet(): boolean {
+    return LEVEL_MAP[this.row][this.col] === 2;
+  }
+
+  isSuperPellet(): boolean {
+    return LEVEL_MAP[this.row][this.col] === 5;
+  }
+
+  isEmpty(): boolean {
+    return LEVEL_MAP[this.row][this.col] === 0;
+  }
+
+  isVoid(): boolean {
+    return LEVEL_MAP[this.row][this.col] === 3;
+  }
+
+  isGhostHouse(): boolean {
+    return LEVEL_MAP[this.row][this.col] === 4;
+  }
+}
 
 export let initialPelletAmount = 0;
 
@@ -400,14 +551,16 @@ function defineComponent(row: number, col: number) {
   }
 }
 
-import { memo, useEffect, useMemo } from 'react';
+import { memo, ReactElement, useEffect, useMemo } from 'react';
 
 const MazeLayer = memo(function MazeLayer() {
   // compute once inside Maze, not on every GamePage render
   const cells = useMemo(
     () =>
       LEVEL_MAP.flatMap((row, r) =>
-        row.map((_, c) => <div key={`${r}-${c}`}>{defineComponent(r, c)}</div>)
+        row.map((_, c) => (
+          <div key={`${r}-${c}`}>{new MazeComponent(r, c).component}</div>
+        ))
       ),
     []
   );
