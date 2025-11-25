8db1a32da0f1f77c1d3643342b463fbe44029b07
Joel Bonini <joel.bonini@gmx.ch>
2025-11-25 08:54:05 +0100

    made math.random work

--- app/core/mazeGenerator.ts
+++ app/core/mazeGenerator.ts
@@ -1,19 +1,7 @@
+'use client';
 import data from '../../public/assets/maze/MazeChunks.json'
 
 
-
-// Creates random number without Math.random()
-function seededRandom(seed: number): Function {
-    let value = seed;
-    return function() {
-        value = (value * 9301 + 49297) % 233280;
-        return value / 233280;
-    };
-}
-
-function randomBetween(random: Function, min: number, max: number): number {
-    return Math.floor(random() + (max - min + 1)) + min;
-}
 
 class Chunk {
     
@@ -33,38 +21,44 @@ class RandomMaze {
     content: number[][];
     width: number;
     height: number;
-    random: Function;
+    seed: number;
 
     constructor(width: number, height: number, seed: number) {
         this.width = width;
         this.height = height;
-        this.random = seededRandom(seed)
+        this.seed = seed;
         this.content = Array(height).fill(null).map(() => Array(width).fill(0));
-        
         this.generate();
+    }
+
+    private random(): number {
+        this.seed = (this.seed * 9301 + 49297) % 233280;
+        return this.seed / 233280;
+    }
+
+    private randomRange(min: number, max: number): number {
+        return Math.floor(this.random() * (max - min + 1)) + min;
     }
 
     generate() {
         this.makeBorder();
-        
-        for (let i = 0; i < 10; i ++) {
+
+        for (let i = 0; i < 10; i++) {
             let chunk: Chunk = this.getRandomChunk();
-            let xRandom: number = randomBetween(this.random, 2, 21);
-            let yRandom: number = randomBetween(this.random, 2, 21);
+            let xRandom: number = Math.floor(this.randomRange(2, 25));
+            let yRandom: number = Math.floor(this.randomRange(2, 21));
             this.insertChunk(xRandom, yRandom, chunk);
         }
-        
     }
 
     makeBorder() {
         this.content[0] = Array(this.width).fill(1);
         this.content[this.content.length - 1] = Array(this.width).fill(1);
 
-        for (let i = 0; i < this.content.length - 1; i ++) {
+        for (let i = 0; i < this.content.length - 1; i++) {
             this.content[i][0] = 1;
             this.content[i][this.content[0].length - 1] = 1;
         }
-        
     }
 
     getRandomChunk(): Chunk {
@@ -73,10 +67,12 @@ class RandomMaze {
     }
 
     insertChunk(startX: number, startY: number, chunk: Chunk) {
-        for (let i = 0; i < chunk.content.length; i ++) {
-            for (let j = 0; j < chunk.content[0].length; j ++) {
+        for (let i = 0; i < chunk.content.length; i++) {
+            for (let j = 0; j < chunk.content[0].length; j++) {
                 if (chunk.content[i][j] === 1) {
-                    this.content[startX + i][startY + j] = chunk.content[i][j];
+                    if (startX + i < this.content.length && startY + j < this.content[0].length) {
+                        this.content[startX + i][startY + j] = chunk.content[i][j];
+                    }
                 }
             }
         }
@@ -85,6 +81,6 @@ class RandomMaze {
 
     
 
-let maze = new RandomMaze(28, 31, 3897);
+let maze = new RandomMaze(28, 31, 6711);
 console.log(maze.content);
 export const LEVEL_MAP_GENERATED: number[][] = maze.content;
